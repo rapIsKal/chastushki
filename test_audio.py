0d3adf33c7ef42fb72685d3c7ca164f599a8b352
vgorelov <rap-is-kal-again@yandex.ru>
2026-03-07 16:11:09 +0300

    test: Prompt user for text input in test_audio.py

--- test_audio.py
+++ test_audio.py
@@ -2,7 +2,7 @@ import sys
 from main import text_to_speech, mix_audio, MELODY_PATH
 
 if __name__ == "__main__":
-    text = sys.stdin.read().strip()
+    text = input("Введите текст")
     tts_path = "test_tts.wav"
     output_path = "test_final.mp3"
     text_to_speech(text, tts_path)
